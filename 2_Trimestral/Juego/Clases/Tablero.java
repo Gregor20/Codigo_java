e2fc07eefed7eefddce4b62e7433e5cc6afd5c27
Grigoriy <gregorct20@gmail.com>
2025-03-24 18:59:09 +0100

    Ej de 2_trimestral (2048) terminado, falta ej zoologico

--- 2_Trimestral/Juego/Clases/Tablero.java
+++ 2_Trimestral/Juego/Clases/Tablero.java
@@ -31,56 +31,56 @@ public class Tablero {
 
 	public void mostrarTablero() {
 		for (int i = 0; i < FIL; i++) {
+			System.out.println();
 			for (int j = 0; j < COL; j++) {
-				System.out.print(tablero[i][j] + " ");
+				System.out.printf("%4d",this.tablero[i][j]);
 			}
 			System.out.println();
 		}
+	}
+	public boolean tableroCompleto() {
+		for (int i = 0; i < FIL; i++) {
+			for (int j = 0; j < COL; j++) {
+				if(tablero[i][j]==0) {
+					return false;
+				}
+			}
+		}return true;
 	}
 
 	public void moverArriba() {
 		for (int j = 0; j < COL; j++) {
 			for (int i = 1; i < FIL; i++) {
-				if (this.tablero[i][j] != 0) {
+				if (this.tablero[i][j] != 0 ) {
 					int ref = i;
-					while (ref > 0 /* && this.tablero[ref-1][j] == 0 */) {
-						this.tablero[ref - 1][j] += this.tablero[ref][j];
+					while (ref > 0  && this.tablero[ref-1][j] == 0 ) {
+							this.tablero[ref - 1][j] = this.tablero[ref][j];
+							this.tablero[ref][j] = 0;
+							ref--;
+					}
+					if(ref > 0 && this.tablero[ref-1][j] == this.tablero[ref][j]) {
+						this.tablero[ref-1][j] += this.tablero[ref][j];
 						this.tablero[ref][j] = 0;
-						ref--;
 					}
 				}
-			}
+		}
 		}
 		rellenarTablero();
 	}
 
 	public void moverAbajo() {
 		for (int j = 0; j < COL; j++) {
-			for (int i = 0; i < FIL; i++) {
+			for (int i = FIL-1; i >= 0; i--) {
 				if (this.tablero[i][j] != 0) {
 					int ref = i;
-					while (ref < 3) {
-						this.tablero[ref + 1][j] += this.tablero[ref][j];
+					while (ref < FIL-1 && this.tablero[ref+1][j]==0) {
+						this.tablero[ref + 1][j] = this.tablero[ref][j];
 						this.tablero[ref][j] = 0;
 						ref++;
 					}
-				}
-			}
-		}
-		rellenarTablero();
-	}
-
-
-
-	public void moverDerecha() {
-		for (int i = 0; i < FIL; i++) {
-			for (int j = 0; j < COL; j++) {
-				if (this.tablero[i][j] != 0) {
-					int ref = j;
-					while (ref < 3) {
-						this.tablero[ref+1][i]+=this.tablero[ref][i];
-						this.tablero[ref][i]=0;
-						ref++;
+					if(ref < FIL-1 && this.tablero[ref+1][j]==this.tablero[ref][j]) {
+						this.tablero[ref+1][j] += this.tablero[ref][j];
+						this.tablero[ref][j]=0;
 					}
 				}
 			}
@@ -88,4 +88,81 @@ public class Tablero {
 		rellenarTablero();
 	}
 
+	public void moverDerecha() {
+		for (int i = 0; i < FIL; i++) {
+			for (int j = COL - 2; j >= 0; j--) {
+				if (this.tablero[i][j] != 0) {
+					int ref = j;
+					while (ref < COL -1 && this.tablero[i][ref +1] == 0) {
+						this.tablero[i][ref + 1] = this.tablero[i][ref];
+						this.tablero[i][ref] = 0;
+						ref++;
+					}
+					if(ref < COL -1 && this.tablero[i][ref +1] == this.tablero[i][ref]) {
+						this.tablero[i][ref +1] += this.tablero[i][ref];
+						this.tablero[i][ref]=0;
+					}
+				}
+			}
+		}
+		rellenarTablero();
+	}
+/*	ESTE MOVER IZQUIERDA ES MOVER DERECHA PERO EL BUCLE DISTINTO
+	public void moverIzquierda() {
+		for (int j = 0; j < FIL; j++) {
+			for (int i = 0; i < COL; i++) {
+				if (this.tablero[j][i] != 0) {
+					int ref = i;
+					while (ref < 3) {
+						this.tablero[j][ref+1] += this.tablero[j][ref];
+						this.tablero[j][ref] = 0;
+						ref++;
+					}
+				}
+			}
+		}
+		rellenarTablero();
+	}
+*/
+	
+	public void moverIzquierda() {
+		for (int i = 0; i < FIL; i++) {
+			for (int j = 1; j < COL; j++) {
+				
+				if (this.tablero[i][j] != 0) {// si la posicion es 2 por ejemplo, haz esto
+					
+					int ref = j;//variable de referencia 
+					
+					while (ref > 0 && this.tablero[i][ref-1]==0) { // mientras que ref sea mayor que 0 y la posicion [0][0] a la que voy a moverme = 0
+						this.tablero[i][ref-1] = this.tablero[i][ref];
+						this.tablero[i][ref] = 0;
+						ref--;
+					}
+					if(ref > 0 && this.tablero[i][ref-1] == this.tablero[i][ref]) {
+						this.tablero[i][ref-1]+=this.tablero[i][ref];
+						this.tablero[i][ref]=0;
+					}
+				}
+			}
+		}
+		rellenarTablero();
+	}
+	
+	public boolean ganador() {
+		for (int i = 0; i < FIL; i++) {
+			for (int j = 0; j < COL; j++) {
+				if(tablero[i][j]==2048)return true;
+			}
+		}return false;
+	}
+	
+	public String finPartida() {
+		if(tableroCompleto()) {
+			return "Tablero completo\n";
+		}else if(ganador()) {
+			return "Has ganado\n";
+		}return "Fin del juego";
+		
+	}
+	
 }
--- 2_Trimestral/Juego/Principal/Inicio_2048.java
+++ 2_Trimestral/Juego/Principal/Inicio_2048.java
@@ -1,81 +1,84 @@
 package Principal;
 
+import java.util.Scanner;
 import Clases.Tablero;
 
 public class Inicio_2048 {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void MenuPrincipal() {
+		System.out.println("Menú Principal\n1. Iniciar partida\n2. Continuar Partida\n" + "3. Salir del juego");
+	}
+	public static void EligeMovimiento() {
+		System.out.println("\nIndica tu movimiento (arriba=w, abajo=s," + " derecha=d, izquierda=a, Menú Principal=m");
+	}
+	public static Tablero IniciarJuego() {
+		Tablero t1 = new Tablero();
+		t1.rellenarTablero();
+		t1.rellenarTablero();
+		return jugar(t1);
+	}
+	public static Tablero ContinuarPartida(Tablero t1) {	
+		return jugar(t1);
+	}
+
+	public static Tablero jugar(Tablero t1) {
+		EligeMovimiento();
+		while (!t1.tableroCompleto() && !t1.ganador()) {
+			String mov = sc.nextLine();
+			switch (mov.toLowerCase()) {
+			case "d":
+				t1.moverDerecha();
+				break;
+			case "w":
+				t1.moverArriba();
+				break;
+			case "s":
+				t1.moverAbajo();
+				break;
+			case "a":
+				t1.moverIzquierda();
+				break;
+			case "m":
+				System.out.println("Has salido al menú principal");
+				return t1;
+			}
+			t1.mostrarTablero();
+			if (t1.ganador()) {
+				System.out.println("\nEnhorabuena, has ganado!");
+			}
+			if (t1.tableroCompleto()) {
+				System.out.println("Tablero lleno, has perdido");
+			}
+		}
+		return null;
+	}
 
 	public static void main(String[] args) {
-		
-		Tablero t1 = new Tablero();
-		
-		/*
-		t1.rellenarTablero();
-		t1.rellenarTablero();
-		t1.mostrarTablero();
-		System.out.println();
-		t1.moverDerecha();
-		t1.mostrarTablero();
-		System.out.println();
-		t1.moverDerecha();
-		t1.mostrarTablero();
-		System.out.println();
-		t1.moverDerecha();
-		t1.mostrarTablero();
-		System.out.println();
-		t1.moverDerecha();
-		t1.mostrarTablero();
-		System.out.println();
-		t1.moverDerecha();
-		t1.mostrarTablero();
-		System.out.println();
-		*/
-		t1.rellenarTablero();
-		t1.rellenarTablero();
-		t1.mostrarTablero();
-		System.out.println();
-		t1.moverAbajo();
-		t1.mostrarTablero();
-		System.out.println();
-		t1.moverAbajo();
-		t1.mostrarTablero();
-		System.out.println();
-		t1.moverAbajo();
-		t1.mostrarTablero();
-		System.out.println();
-		t1.moverAbajo();
-		t1.mostrarTablero();
-		System.out.println();
-		t1.moverAbajo();
-		t1.mostrarTablero();
-		System.out.println();
-		
-		t1.moverArriba();
-		t1.mostrarTablero();
-		
-		/*
-		t1.moverArriba();
-		
-		t1.mostrarTablero();
-		System.out.println();
-		t1.moverArriba();
-		t1.mostrarTablero();
-		System.out.println();
-		t1.moverArriba();
-		t1.mostrarTablero();
-		System.out.println();
-		t1.moverArriba();
-		t1.mostrarTablero();
-		System.out.println();
-		t1.moverArriba();
-		t1.mostrarTablero();
-		System.out.println();
-		t1.moverArriba();
-		t1.mostrarTablero();
-		System.out.println();
-		t1.moverArriba();
-		t1.mostrarTablero();
-		*/
 
+		System.out.println("Bienvenido al juego 2048!");
+		Tablero t1 = null;
+
+		while (true) {
+			int eleccion = 0;
+			MenuPrincipal();
+			eleccion = sc.nextInt();
+			switch (eleccion) {
+			case 1:
+				t1 = IniciarJuego();
+				break;
+			case 2:
+				if(t1!=null) {
+					t1 = ContinuarPartida(t1);
+				}else{
+					System.out.println("No hay ninguna partida comenzada");
+				}
+				break;
+			case 3:
+				System.out.println("Juego terminado");
+				return;
+			}
+		}
 	}
 
 }
